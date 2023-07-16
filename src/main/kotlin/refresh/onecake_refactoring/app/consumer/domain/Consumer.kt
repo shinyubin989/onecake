0d1fb818025728cfeff49bf98b1e2af4f72f7dd9
yubin <dbqls2821@gmail.com>
2023-07-16 17:44:42 +0900

    feat: add some properties to consumer data class

--- src/main/kotlin/refresh/onecake_refactoring/app/consumer/domain/Consumer.kt
+++ src/main/kotlin/refresh/onecake_refactoring/app/consumer/domain/Consumer.kt
@@ -1,6 +1,9 @@
 package refresh.onecake_refactoring.app.consumer.domain
 
 data class Consumer (
+    val name: String,
+    val phoneNumber: String,
+    val profileImg: String?,
     val deleted : Boolean
 )
 
