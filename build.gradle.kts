ba9740bb8dd076479e9b9296e20125fa70b6aa2d
yubin <dbqls2821@gmail.com>
2023-07-17 22:32:38 +0900

    dependency: added kotlin all-open, kotlin no-arg, jwt, webflux, spring rest doc dependencies...etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,23 @@ plugins {
 	id("org.springframework.boot") version "3.1.1"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
+	id("org.jetbrains.kotlin.plugin.noarg") version "1.9.0"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.9.0"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
 }
 
 group = "refresh"
@@ -23,14 +37,35 @@ repositories {
 val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// spring-boot-web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// feign
+//	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+	// webflux
+	implementation("org.springframework:spring-webflux:6.0.6")
+
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// spring-restdocs
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+	// db, jpa
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<KotlinCompile> {
